3510a93834212f1c5acb34c2aeaf2a653786e38c
Matome Makgalemela <schroodinger@gmail.com>
2019-10-30 00:20:41 +0200

    Sockets deployment

--- Channels/asgi.py
+++ Channels/asgi.py
@@ -5,10 +5,9 @@ defined in the ASGI_APPLICATION setting.
 
 import os
 import django
-# import channels.layers
 from channels.routing import get_default_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Channels.settings")
+import channels.layers
 channel_layer = channels.layers.get_channel_layer()
 django.setup()
 application = get_default_application()
