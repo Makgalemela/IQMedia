bf00f2994c154f9954247d20b93fb86c53e7612c
Matome Makgalemela <schroodinger@gmail.com>
2019-10-29 22:02:38 +0200

    Sockets deployment

--- Echo/admin.py
+++ Echo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+# web2: daphne channels.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+# worker: python manage.py runworker -v2
