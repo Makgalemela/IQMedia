851ccabcb7a038b5292c14255f683e3625089945
Matome Makgalemela <schroodinger@gmail.com>
2019-10-30 03:16:40 +0200

    Modified the channel urls

--- Channels/settings.py
+++ Channels/settings.py
@@ -138,13 +138,7 @@ CHANNEL_LAYERS = {
     },
 }
 
-
-
-
-
-#LOGIN_REDIRECT_URL = "users/temp.html"
-
-
+LOGIN_REDIRECT_URL = "users/temp.html"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL ='/'
 LOGIN_URL ='user_login'
