d980304b623650cf8f8e210d6108af06c013ccb6
Matome Makgalemela <schroodinger@gmail.com>
2019-10-30 04:55:02 +0200

    Modified the channel urls

--- Channels/routing.py
+++ Channels/routing.py
@@ -1,9 +1,10 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from Echo.consumers import NoseyConsumer
+from django.conf.urls import url
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
-        path('ws/', NoseyConsumer),
+        url(r'^ws/', NoseyConsumer),
     ])
 })
