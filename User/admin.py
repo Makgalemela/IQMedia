5ebdf8af483d83a3b1d638691d9c596161b81fa0
Matome Makgalemela <schroodinger@gmail.com>
2019-10-30 04:12:10 +0200

    Modified the channel urls

--- User/admin.py
+++ User/admin.py
@@ -18,7 +18,7 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'usertype')
+    list_display = ('email', 'usertype', )
     list_filter = ('usertype',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
