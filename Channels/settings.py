b9e026b23ee78bca7142c800a1af5bbb7f0ca50b
Matome Makgalemela <schroodinger@gmail.com>
2019-10-30 03:27:00 +0200

    Modified the channel urls

--- Channels/settings.py
+++ Channels/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'Channels.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'User/templates/registration'), os.path.join(BASE_DIR, 'User/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
